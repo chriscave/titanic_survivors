81d953efd8ee1a014a06f50a2e9fda781e083dca
Chris on Bournemouth v3 <chriscave89@gmail.com>
2018-02-21 17:40:28 +0100

    fixing back prop

--- BackProp.m
+++ BackProp.m
@@ -1,21 +1,18 @@
 function [D1,D2] = BackProp(X,y,theta1,theta2,lambda);
   [m n] = size(X);
-  [r1 r2] = size(theta2)
+  
   
   
   
   [a2, a3] = hyp(X, theta1, theta2);
-  [s1 s2] = size(a2);
   delta3 = a3 - y';
-  theta2 = theta2(:,[2:r2]);
-  a2 = a2([2:s2],:]);
   delta2 = (theta2' * delta3).*a2.*(1-a2);
+  delta2([1],:) = [];
   
   Delta1 = delta2 * X;
   Delta2 = delta3 * a2';
   [m1 n1] = size(Delta1);
   [m2 n2] = size(Delta2);
-  
   D1 = zeros(m1, n1);
   D2 = zeros(m2, n2);
   
