1e20cd4145ed211e8f51bc77e68ec4568359a383
Chris on Bournemouth v3 <chriscave89@gmail.com>
2018-02-21 17:29:31 +0100

    editing back prop

--- BackProp.m
+++ BackProp.m
@@ -1,24 +1,29 @@
-function [D1 D2] = BackProp(X,y,theta1,theta2,lambda);
+function [D1,D2] = BackProp(X,y,theta1,theta2,lambda);
   [m n] = size(X);
-  [m1 n1] = size(theta1);
-  [m2 n2] = size(theta2);
+  [r1 r2] = size(theta2)
+  
+  
+  
+  [a2, a3] = hyp(X, theta1, theta2);
+  [s1 s2] = size(a2);
+  delta3 = a3 - y';
+  theta2 = theta2(:,[2:r2]);
+  a2 = a2([2:s2],:]);
+  delta2 = (theta2' * delta3).*a2.*(1-a2);
+  
+  Delta1 = delta2 * X;
+  Delta2 = delta3 * a2';
+  [m1 n1] = size(Delta1);
+  [m2 n2] = size(Delta2);
   
   D1 = zeros(m1, n1);
   D2 = zeros(m2, n2);
-  Delta1 = zeros(m1, n1);
-  Delta2 = zeros(m2, n2);
   
-  [a2 a3] = hyp(X, theta1, theta2);
-  delta3 = a3 - y;
-  delta2 = (theta2' * delta3).*a2.*(1-a2);
   
-  Delta1 = Delta1 + delta2 * a1';
-  Delta2 = Delta2 + delta3 * a2';
-  
-  E1 = 1/m * ( Delta1(:,[2,n1]) + lambda theta1(:,[2:n1]));
+  E1 = 1/m * ( Delta1(:,[2:n1]) + lambda * theta1(:,[2:n1]));
   e1 = 1/m * ( Delta1(:,[1]));
   
-  E2 = 1/m * ( Delta2(:,[2,n2]) + lambda theta2(:,[2:n2]));
+  E2 = 1/m * ( Delta2(:,[2:n2]) + lambda * theta2(:,[2:n2]));
   e2 = 1/m * ( Delta2(:,[1]));
   
   D1 = [e1 , E1];
--- Cost.m
+++ Cost.m
@@ -1,11 +1,11 @@
 function J = Cost(theta1, theta2, X,y,lambda);
   J = 0;
-  h = hyp(X,theta1,theta2);
-  
+  [a2, a3] = hyp(X,theta1,theta2);
+  [m n] = size(X);
   [m1 n1] = size(theta1);
   [m2 n2] = size(theta2);
   theta1_reg = theta1(:, [2:n1]);
   theta2_reg = theta2(:, [2:n2]);
-  lambda_reg = ((lambda/2) *( sum(sumsq(theta1_reg)) + sum(sumsq(theta2_reg)));
-  J = 1/m * ((-y' * log(h)) - ((1 - y)' * log( 1 - h)) +lambda_reg);
+  lambda_reg = (lambda/2) *( sum(sumsq(theta1_reg)) + sum(sumsq(theta2_reg)));
+  J = 1/m * ((-y' * log(a3)) - ((1 - y)' * log( 1 - a3)) +lambda_reg);
 end
