e371088bf0ecec28d7232f76b1a43c599847d8db
chris <chriscave89@gmail.com>
2018-05-31 15:53:57 +0200

    adding Back Propagation

--- BackProp.m
+++ BackProp.m
@@ -1,28 +1,38 @@
-function [D1,D2] = BackProp(X,y,theta1,theta2,lambda);
-  [m n] = size(X);  
+function D = BackProp(X,y,theta,lambda,rowct,colct);
+ #extracting the number of nodes in neural network
+ r = size(rowct,2);
+#  hl = zeros(1, (size(rowct,2) - 2));
+#  for i= 1:size(rowct,2) - 2;
+#    hl(i) = rowct(i+1) - rowct(i);
+#  end
+  hyprowct = [1];
+  for i = 2:r;
+    k = hyprowct(i-1) + (rowct(i)-rowct(i-1)) + 1;
+    hyprowct = [hyprowct,k];
+  end
+  H = hyp(X, theta, rowct, colct);
+  D = [];
+  delta = H(hyprowct(size(rowct,2) - 1),:) - y';
+    for i = (r - 1):-1:2;
+      Delta = delta * H((hyprowct(i-1): hyprowct(i) -1),:)';
+      theta_ = theta((rowct(i):rowct(i+1) - 1), (colct(i):colct(i+1) - 1));
+    
+      D_ = 1/(size(X,1)) * (Delta(:,(2:size(Delta,2))) + lambda * theta_(:,(2:size(Delta,2))));
+      d_ = 1/(size(X,1)) * (Delta(:,1));
+    
+      D_ = [d_,D_];
+      D = blkdiag(D_,D);
+   
+      A = (theta_' * delta);
+      delta = A.* H(hyprowct(i-1): hyprowct(i) - 1, :) .* (1 - H(hyprowct(i-1): hyprowct(i) - 1, :) );
+      delta(1,:) = [];
+    end
+    Delta = delta * X;
+    theta_ = theta((rowct(1):rowct(2) - 1), (colct(1):colct(2) - 1));
+    D_ = 1/(size(X,1)) * (Delta(:,(2:size(Delta,2))) + lambda * theta_(:,(2:size(Delta,2))));
+    d_ = 1/(size(X,1)) * (Delta(:,1));    
+    D_ = [d_,D_];
+    D = blkdiag(D_,D);
   
-  [a2, a3] = hyp(X, theta1, theta2); #forward propagation
-  
-  delta3 = a3 - y';
-  
-  delta2 = (theta2' * delta3).*a2.*(1-a2);
-  delta2([1],:) = []; #when finding the derivatives, one does not find the derivative of theta_{0,j} because it does not exist, so we delete the first row.
-  
-  Delta1 = delta2 * X;
-  Delta2 = delta3 * a2';
-  [m1 n1] = size(Delta1);
-  [m2 n2] = size(Delta2);
-  D1 = zeros(m1, n1);
-  D2 = zeros(m2, n2);
-  
-  
-  E1 = 1/m * ( Delta1(:,[2:n1]) + lambda * theta1(:,[2:n1]));
-  e1 = 1/m * ( Delta1(:,[1]));
-  
-  E2 = 1/m * ( Delta2(:,[2:n2]) + lambda * theta2(:,[2:n2]));
-  e2 = 1/m * ( Delta2(:,[1]));
-  
-  D1 = [e1 , E1];
-  D2 = [e2, E2];
-end
+end  
   
