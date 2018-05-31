94a17a668840a9f547a9759e81ee72df30455f3f
chris <chriscave89@gmail.com>
2018-05-31 15:55:08 +0200

    adding gradient  descent algorithm

--- gradientDescent.m
+++ gradientDescent.m
@@ -1,13 +1,10 @@
-function [theta, Cost_history] = gradientDescent(X, y, theta1, theta2, lambda, alpha, num_iters);
-  Cost_history = zeros(num_iters, 1);
-  [m1 n1] = size(theta1);
-  [m2 n2] = size(theta2);
-  for iter = 1:num_iters;
-    theta = blkdiag(theta1, theta2);
-    [D1, D2] = BackProp(X,y,theta1, theta2, lambda);
-    theta = theta - alpha * blkdiag(D1,D2);
-    theta1 = theta([1:m1],[1:n1]);
-    theta2 = theta([m1 + 1: m1 + m2], [n1 + 1: n1 + n2]);
-    Cost_history(iter) = Cost(theta1, theta2, X,y,lambda);
+function [theta, Cost_history] = gradientDescent(X,y,theta,lambda,alpha,num_iters,rowct,colct);
+  Cost_history = [];
+  r =size(rowct,2);
+  
+  for iter = 1:num_iters;    
+    D = BackProp(X,y,theta,lambda,rowct,colct);    
+    theta = theta - alpha * D;
+    Cost_history = [Cost_history;Cost(X,y,theta,lambda,rowct,colct)'];
   end
 end
